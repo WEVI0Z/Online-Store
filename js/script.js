5d3cbc0758ccfc5c05dcb5f622da2afa5b1217c6
Wevioz <wevioz@mail.ru>
2022-05-15 14:32:19 +0300

    Done the cards render func

--- js/script.js
+++ js/script.js
@@ -105,7 +105,7 @@ function createAdvertsArr(arr) {
       const photosArr = []
   
       for(let i = 0; i < photosNum; i++) {
-        photosArr.push(PHOTOS[getRandomIndex(PHOTOS)]);
+        photosArr.push("img/" + PHOTOS[getRandomIndex(PHOTOS)]);
       }
   
       return photosArr;
@@ -136,7 +136,7 @@ function createAdvertsArr(arr) {
     return object;
   }  
 
-  for(let i = 0; i < 7; i++) {
+  for(let i = 0; i < 9; i++) {
     arr.push(createAdvertData());
   }
 
@@ -151,30 +151,36 @@ function insertMarkup(wrapper, markup) {
 function renderAdverts() {
   const advertsList = document.querySelector(".results__list");
 
-  const advertsMarkup = advertsData.map(() => {
-    return `
-    <li class="results__item product">
-    <button class="product__favourite fav-add" type="button" aria-label="Добавить в избранное">
-      <svg width="22" height="20" viewBox="0 0 22 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path fill-rule="evenodd" clip-rule="evenodd" d="M3 7C3 13 10 16.5 11 17C12 16.5 19 13 19 7C19 4.79086 17.2091 3 15 3C12 3 11 5 11 5C11 5 10 3 7 3C4.79086 3 3 4.79086 3 7Z" stroke="white" stroke-width="2" stroke-linejoin="round"/>
-      </svg>
-    </button>
-    <div class="product__image">
-      <div class="product__image-more-photo hidden">+2 фото</div>
-      <img src="img/house_4.png" width="318" height="220" alt="Загородный дом с видом на озеро">
-    </div>
-    <div class="product__content">
-      <h3 class="product__title">
-        <a href="#">Загородный дом с видом на озеро</a>
-      </h3>
-      <div class="product__price">3 000 000 ₽</div>
-      <div class="product__address">Приозёрск, улица Прибрежная</div>
-      <div class="product__date">Сегодня</div>
-    </div>
-  </li>`;
-
-  insertMarkup(advertsList, advertsMarkup);
+  const advertsMarkup = advertsData.map((itAdvert, itIndex) => {
+    if(itIndex < 7) {
+      return `
+        <li class="results__item product">
+        <button class="product__favourite fav-add" type="button" aria-label="Добавить в избранное">
+          <svg width="22" height="20" viewBox="0 0 22 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M3 7C3 13 10 16.5 11 17C12 16.5 19 13 19 7C19 4.79086 17.2091 3 15 3C12 3 11 5 11 5C11 5 10 3 7 3C4.79086 3 3 4.79086 3 7Z" stroke="white" stroke-width="2" stroke-linejoin="round"/>
+          </svg>
+        </button>
+        <div class="product__image">
+          <div class="product__image-more-photo hidden">+${itAdvert.photos.length - 1} фото</div>
+          <img src="${itAdvert.photos[0]}" width="318" height="220" alt="${itAdvert.name}">
+        </div>
+        <div class="product__content">
+          <h3 class="product__title">
+            <a href="#">${itAdvert.name}</a>
+          </h3>
+          <div class="product__price">${itAdvert.price} ₽</div>
+          <div class="product__address">${itAdvert.address.city}, ${itAdvert.address.street}</div>
+          <div class="product__date">${itAdvert.publishDate.getDate() < 10 ? "0" + itAdvert.publishDate.getDate() : itAdvert.publishDate.getDate()}.${itAdvert.publishDate.getMonth() < 10 ? "0" + itAdvert.publishDate.getMonth() : itAdvert.publishDate.getMonth()}.${itAdvert.publishDate.getFullYear()}</div>
+        </div>
+      </li>`;
+    }
+    return ;
   })
+
+  insertMarkup(advertsList, advertsMarkup.join(""));
 }
 
-console.log(createAdvertsArr(advertsData));
+createAdvertsArr(advertsData);
+renderAdverts();
+
+console.log(advertsData)
