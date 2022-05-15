0282c6f0829801a1a2bc90b020287e4a1ddbb518
Wevioz <wevioz@mail.ru>
2022-05-15 16:10:35 +0300

    Done the rendering of the popUpAdvert

--- js/script.js
+++ js/script.js
@@ -121,6 +121,7 @@ function createAdvertsArr(arr) {
         rating: getRandomNum(MIN_RATING, MAX_RATING * 10) / 10
       },
       publishDate: new Date(currentDate.getFullYear(), getRandomNum(1, currentDate.getMonth()), getRandomNum(1, currentDate.getDate())),
+      // publishDate: new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate() - 1),
       address: {
         city: CITIES[getRandomIndex(CITIES)],
         street: STREETS[getRandomIndex(STREETS)],
@@ -148,13 +149,33 @@ function insertMarkup(wrapper, markup) {
   wrapper.insertAdjacentHTML("afterBegin", markup);
 }
 
+function makeUpTheDate(dateObj) {
+  let dateStr = "";
+
+  const dateDay = dateObj.getDate();
+  const dateMonth = dateObj.getMonth();
+  const dateYear = dateObj.getFullYear();
+
+  if(dateDay == currentDate.getDate() && dateMonth == currentDate.getMonth() && dateYear == currentDate.getFullYear()) {
+    return "Сегодня";
+  } else if (dateDay == currentDate.getDate() - 1 && dateMonth == currentDate.getMonth() && dateYear == currentDate.getFullYear()) {
+    return "Вчера";
+  }
+
+  dateStr += dateDay < 10 ? "0" + dateDay + "." : dateDay + ".";
+  dateStr += dateObj.getMonth() < 10 ? "0" + (dateObj.getMonth() + 1) : (dateObj.getMonth() + 1);
+  dateStr += "." + dateObj.getFullYear();
+
+  return dateStr;
+}
+
 function renderAdverts() {
   const advertsList = document.querySelector(".results__list");
 
   const advertsMarkup = advertsData.map((itAdvert, itIndex) => {
     if(itIndex < 7) {
       return `
-        <li class="results__item product">
+        <li class="results__item product" data-index="${itIndex}">
         <button class="product__favourite fav-add" type="button" aria-label="Добавить в избранное">
           <svg width="22" height="20" viewBox="0 0 22 20" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path fill-rule="evenodd" clip-rule="evenodd" d="M3 7C3 13 10 16.5 11 17C12 16.5 19 13 19 7C19 4.79086 17.2091 3 15 3C12 3 11 5 11 5C11 5 10 3 7 3C4.79086 3 3 4.79086 3 7Z" stroke="white" stroke-width="2" stroke-linejoin="round"/>
@@ -170,7 +191,7 @@ function renderAdverts() {
           </h3>
           <div class="product__price">${itAdvert.price} ₽</div>
           <div class="product__address">${itAdvert.address.city}, ${itAdvert.address.street}</div>
-          <div class="product__date">${itAdvert.publishDate.getDate() < 10 ? "0" + itAdvert.publishDate.getDate() : itAdvert.publishDate.getDate()}.${itAdvert.publishDate.getMonth() < 10 ? "0" + itAdvert.publishDate.getMonth() : itAdvert.publishDate.getMonth()}.${itAdvert.publishDate.getFullYear()}</div>
+          <div class="product__date">${makeUpTheDate(itAdvert.publishDate)}</div>
         </div>
       </li>`;
     }
@@ -180,7 +201,97 @@ function renderAdverts() {
   insertMarkup(advertsList, advertsMarkup.join(""));
 }
 
+function popUpAdvertControl() {
+  const advertsList = document.querySelector(".results__list");
+
+  const popUpSection = document.querySelector(".popup");
+  const popUpCloseButton = popUpSection.querySelector(".popup__close");
+  const popUpDateBlock = popUpSection.querySelector(".popup__date");
+  const popUpNameBlock = popUpSection.querySelector(".popup__title");
+  const popUpPriceBlock = popUpSection.querySelector(".popup__price");
+  const popUpMainImgBlock = popUpSection.querySelector(".gallery__main-pic img");
+  const popUpOtherImgBlock = popUpSection.querySelector(".gallery__list");
+  const popUpCharsBlocks = popUpSection.querySelectorAll(".popup__chars .chars__item");
+  const popUpSellerNameBlock = popUpSection.querySelector(".seller__name");
+  const popUpSellerRatingBlock = popUpSection.querySelector(".seller__rating span");
+  const popUpDescriptionBlock = popUpSection.querySelector(".popup__description p");
+  const popUpAddressBlock = popUpSection.querySelector(".popup__address");
+
+  function hangAllHandlers() {
+    popUpCloseButton.addEventListener("click", closeButtonClickHandler);
+  }
+
+  function removeAllHandlers() {
+    popUpCloseButton.removeEventListener("click", closeButtonClickHandler);
+  }
+
+  function closePopUp() {
+    removeAllHandlers();
+
+    popUpSection.style.display = "none";
+  }
+
+  function createOtherImgsBlock(advert) {
+    return advert.photos.map((itPhoto) => {
+      return `  
+      <li class="gallery__item">
+        <img src="${itPhoto}" width="124" height="80" alt="${advert.name}">
+      </li>
+      `
+    }).join("");
+  }
+
+  function openPopUp(advert) {
+    hangAllHandlers();
+    popUpDateBlock.textContent = makeUpTheDate(advert.publishDate);
+    popUpNameBlock.textContent = advert.name;
+    popUpPriceBlock.textContent = advert.price;
+    popUpMainImgBlock.src = advert.photos[0];
+    popUpSellerNameBlock.textContent = advert.seller.fullname;
+    popUpSellerRatingBlock.textContent = advert.seller.rating;
+    popUpDescriptionBlock.textContent = advert.description;
+    popUpAddressBlock.textContent = advert.address.city + ", " + advert.address.street + " дом " + advert.address.building;
+
+    popUpCharsBlocks.forEach((itBlock) => {
+      const value = itBlock.querySelector(".chars__value");
+
+      switch(itBlock.dataset.filters) {
+        case "area":
+          value.textContent = advert.filters.area;
+          break;
+        case "roomsCount":
+          value.textContent = advert.filters.roomsCount;
+          break;
+        case "type":
+          value.textContent = advert.filters.type;
+      } 
+    });
+
+    insertMarkup(popUpOtherImgBlock, createOtherImgsBlock(advert));
+
+    popUpSection.style.display = "block";
+  }
+
+  function cardsClickHandler(evt) {
+    evt.preventDefault();
+
+    const targetAdvert = evt.target.closest("li");
+    const advertData = advertsData[targetAdvert.dataset.index];
+
+    openPopUp(advertData);
+  }
+
+  function closeButtonClickHandler(evt) {
+    evt.preventDefault();
+
+    closePopUp();
+  }
+
+  advertsList.addEventListener("click", cardsClickHandler);
+}
+
 createAdvertsArr(advertsData);
 renderAdverts();
+popUpAdvertControl();
 
-console.log(advertsData)
+console.log(advertsData);
